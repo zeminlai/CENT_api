93362dd272828e7854a5b3b474855dc99803e1fb
Ck Yong <118044421+ckyong826@users.noreply.github.com>
2025-01-09 22:27:52 +0800

    Migrations

--- cmd/storage/db.go
+++ cmd/storage/db.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -25,7 +27,6 @@ func InitDB() {
 	dbName := os.Getenv("DB_NAME")
 
 	db, err = sql.Open("postgres", fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPass, dbName, dbPort))
-
 	if err != nil {
 		panic(err.Error())
 	}
@@ -36,6 +37,27 @@ func InitDB() {
 	}
 
 	fmt.Println("Successfully connected to database")
+
+	// Automate migrations
+	migrateDB()
+}
+
+func migrateDB() {
+	// Create a new migrate instance
+	m, err := migrate.New(
+		"file://cmd/storage/migrations",
+		fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME")),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create migrate instance: %v", err)
+	}
+
+	// Perform the migration
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("Failed to apply migrations: %v", err)
+	}
+
+	fmt.Println("Database migrations applied successfully")
 }
 
 func GetDB() *sql.DB {
--- go.mod
+++ go.mod
@@ -12,11 +12,15 @@ require (
 require (
 	github.com/andybalholm/cascadia v1.3.3 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.18.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
